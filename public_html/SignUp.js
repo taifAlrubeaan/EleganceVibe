492d8e7ee2651df0ea2334a19a5e3215a1b0b6ff
leenaksu <146577541+leenaksu@users.noreply.github.com>
2024-02-02 22:54:32 +0300

    Create SignUp.js

---
+++ public_html/SignUp.js
@@ -0,0 +1,20 @@
+function showForm(userType) {
+      document.getElementById('designer-form').style.display = userType === 'designer' ? 'block' : 'none';
+      document.getElementById('client-form').style.display = userType === 'client' ? 'block' : 'none';
+}
+
+
+  function submitdForm() {
+  //userType.preventDefault();
+  
+  // Redirect to the designer's homepage
+  window.location.assign("Designer.html");
+}
+
+function submitcForm() {
+ // userType.preventDefault();
+  
+  // Redirect to the client's homepage
+  window.location.href = "Client.html";
+}
+  
